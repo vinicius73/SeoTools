9b43d3f2e9543f2a3b77ffcc627a400be7eefa5d
Vinicius <luiz.vinicius73@gmail.com>
2013-12-29 19:24:20 -0200

    reindented

--- src/Calotype/SEO/Providers/SEOServiceProvider.php
+++ src/Calotype/SEO/Providers/SEOServiceProvider.php
@@ -1,12 +1,12 @@
 <?php namespace Calotype\SEO\Providers;
 
-use Illuminate\Http\Response;
-use Illuminate\Support\ServiceProvider;
-
 use Calotype\SEO\Generators\MetaGenerator;
+use Calotype\SEO\Generators\OpenGraphGenerator;
 use Calotype\SEO\Generators\RobotsGenerator;
 use Calotype\SEO\Generators\SitemapGenerator;
-use Calotype\SEO\Generators\OpenGraphGenerator;
+use Illuminate\Http\Response;
+
+use Illuminate\Support\ServiceProvider;
 
 class SEOServiceProvider extends ServiceProvider
 {
@@ -45,30 +45,40 @@ class SEOServiceProvider extends ServiceProvider
         $this->app['calotype.seo.generators.robots']->addSitemap($this->app['request']->root() . '/sitemap.xml');
 
         // Generate sitemap.xml route
-        $this->app['router']->get('sitemap.xml', function () use ($app) {
-                if($app['config']->get('seo::sitemap.cache')):
-                    $sitemap = $app['cache']->remember('seo::sitemap.xml',$app['config']->get('seo::sitemap.cachetime'),function() use($app){
+        $this->app['router']->get(
+            'sitemap.xml',
+            function () use ($app) {
+                if ($app['config']->get('seo::sitemap.cache')):
+                    $sitemap = $app['cache']->remember(
+                        'seo::sitemap.xml',
+                        $app['config']->get('seo::sitemap.cachetime'),
+                        function () use ($app) {
                             $app['calotype.seo.generators.sitemap.run']->run();
                             return $app['calotype.seo.generators.sitemap']->generate();
-                        });
+                        }
+                    );
                 else:
                     $app['calotype.seo.generators.sitemap.run']->run();
                     $sitemap = $app['calotype.seo.generators.sitemap']->generate();
                 endif;
 
-            $response = new Response($sitemap, 200);
-            $response->header('Content-Type', 'text/xml');
+                $response = new Response($sitemap, 200);
+                $response->header('Content-Type', 'text/xml');
 
-            return $response;
-        });
+                return $response;
+            }
+        );
 
         // Generate robots.txt route
-        $this->app['router']->get('robots.txt', function () use ($app) {
-            $response = new Response($app['calotype.seo.generators.robots']->generate(), 200);
-            $response->header('Content-Type', 'text/plain');
+        $this->app['router']->get(
+            'robots.txt',
+            function () use ($app) {
+                $response = new Response($app['calotype.seo.generators.robots']->generate(), 200);
+                $response->header('Content-Type', 'text/plain');
 
-            return $response;
-        });
+                return $response;
+            }
+        );
     }
 
     /**
@@ -79,30 +89,45 @@ class SEOServiceProvider extends ServiceProvider
     public function registerBindings()
     {
         // Register the sitemap.xml generator
-        $this->app->singleton('calotype.seo.generators.sitemap', function ($app) {
-            return new SitemapGenerator();
-        });
+        $this->app->singleton(
+            'calotype.seo.generators.sitemap',
+            function ($app) {
+                return new SitemapGenerator();
+            }
+        );
 
         // Register the sitemap configuration
-        $this->app->singleton('calotype.seo.generators.sitemap.run', function ($app) {
-            $class = $app['config']->get('seo::sitemap.classrun');
-            return new $class($app['calotype.seo.generators.sitemap']);
-        });
+        $this->app->singleton(
+            'calotype.seo.generators.sitemap.run',
+            function ($app) {
+                $class = $app['config']->get('seo::sitemap.classrun');
+                return new $class($app['calotype.seo.generators.sitemap']);
+            }
+        );
 
         // Register the meta tags generator
-        $this->app->singleton('calotype.seo.generators.meta', function ($app) {
-            return new MetaGenerator($app['config']->get('seo::meta.defaults'));
-        });
+        $this->app->singleton(
+            'calotype.seo.generators.meta',
+            function ($app) {
+                return new MetaGenerator($app['config']->get('seo::meta.defaults'));
+            }
+        );
 
         // Register the robots.txt generator
-        $this->app->singleton('calotype.seo.generators.robots', function ($app) {
-            return new RobotsGenerator();
-        });
+        $this->app->singleton(
+            'calotype.seo.generators.robots',
+            function ($app) {
+                return new RobotsGenerator();
+            }
+        );
 
         // Register the open graph properties generator
-        $this->app->singleton('calotype.seo.generators.opengraph', function ($app) {
-            return new OpenGraphGenerator();
-        });
+        $this->app->singleton(
+            'calotype.seo.generators.opengraph',
+            function ($app) {
+                return new OpenGraphGenerator();
+            }
+        );
     }
 
     /**
