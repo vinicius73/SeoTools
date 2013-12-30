cfa77a0bc9ef32da0f394422a62bae1995ed38f5
Vinicius <luiz.vinicius73@gmail.com>
2013-12-30 14:47:47 -0200

    reident

--- src/Vinicius73/SEO/Generators/OpenGraphGenerator.php
+++ src/Vinicius73/SEO/Generators/OpenGraphGenerator.php
@@ -4,106 +4,113 @@ use Vinicius73\SEO\Contracts\OpenGraphAware;
 
 class OpenGraphGenerator
 {
-    /**
-     * The prefix used by the open graph protocol.
-     *
-     * @const OPENGRAPH_PREFIX
-     */
-    const OPENGRAPH_PREFIX = 'og:';
+	/**
+	 * The prefix used by the open graph protocol.
+	 *
+	 * @const OPENGRAPH_PREFIX
+	 */
+	const OPENGRAPH_PREFIX = 'og:';
 
-    /**
-     * The tag used by the open graph protocol.
-     *
-     * @const OPENGRAPH_TAG
-     */
-    const OPENGRAPH_TAG = '<meta property="[property]" content="[value]" />';
+	/**
+	 * The tag used by the open graph protocol.
+	 *
+	 * @const OPENGRAPH_TAG
+	 */
+	const OPENGRAPH_TAG = '<meta property="[property]" content="[value]" />';
 
-    /**
-     * The properties that we are going to generate.
-     *
-     * @var array
-     */
-    protected $properties = array();
+	/**
+	 * The properties that we are going to generate.
+	 *
+	 * @var array
+	 */
+	protected $properties = array();
 
-    /**
-     * The properties that are required.
-     *
-     * @var array
-     */
-    protected $required = array(
-        'title', 'type', 'image', 'url', 'site_name'
-    );
+	/**
+	 * The properties that are required.
+	 *
+	 * @var array
+	 */
+	protected $required = array(
+		'title',
+		'type',
+		'image',
+		'url',
+		'site_name'
+	);
 
-    /**
-     * Render the open graph tags.
-     *
-     * @return string
-     */
-    public function generate()
-    {
-        $html = array();
+	/**
+	 * Render the open graph tags.
+	 *
+	 * @return string
+	 */
+	public function generate()
+	{
+		$html = array();
 
-        foreach ($this->properties as $property => $value) {
-            $html[] = strtr(static::OPENGRAPH_TAG, array(
-                '[property]' => static::OPENGRAPH_PREFIX . $property,
-                '[value]' => $value
-            ));
-        }
+		foreach ($this->properties as $property => $value) {
+			$html[] = strtr(
+				static::OPENGRAPH_TAG,
+				array(
+					'[property]' => static::OPENGRAPH_PREFIX . $property,
+					'[value]'    => $value
+				)
+			);
+		}
 
-        return implode(PHP_EOL, $html);
-    }
+		return implode(PHP_EOL, $html);
+	}
 
-    /**
-     * Set the open graph properties from a raw array.
-     *
-     * @param array $properties
-     */
-    public function fromRaw($properties)
-    {
-        $this->validateProperties($properties);
+	/**
+	 * Set the open graph properties from a raw array.
+	 *
+	 * @param array $properties
+	 */
+	public function fromRaw($properties)
+	{
+		$this->validateProperties($properties);
 
-        foreach ($properties as $property => $value) {
-            $this->properties[$property] = $value;
-        }
-    }
+		foreach ($properties as $property => $value) {
+			$this->properties[$property] = $value;
+		}
+	}
 
-    /**
-     * Use the open graph data of a open graph aware object.
-     *
-     * @param OpenGraphAware $object
-     */
-    public function fromObject(OpenGraphAware $object)
-    {
-        $properties = $object->getOpenGraphData();
+	/**
+	 * Use the open graph data of a open graph aware object.
+	 *
+	 * @param OpenGraphAware $object
+	 */
+	public function fromObject(OpenGraphAware $object)
+	{
+		$properties = $object->getOpenGraphData();
 
-        $this->validateProperties($properties);
+		$this->validateProperties($properties);
 
-        foreach ($properties as $property => $value) {
-            $this->properties[$property] = $value;
-        }
-    }
+		foreach ($properties as $property => $value) {
+			$this->properties[$property] = $value;
+		}
+	}
 
-    /**
-     * Reset all the properties.
-     *
-     * @return void
-     */
-    public function reset()
-    {
-        $this->properties = array();
-    }
+	/**
+	 * Reset all the properties.
+	 *
+	 * @return void
+	 */
+	public function reset()
+	{
+		$this->properties = array();
+	}
 
-    /**
-     * Validate to make sure the properties contain all required ones.
-     *
-     * @param array $properties
-     */
-    protected function validateProperties($properties)
-    {
-        foreach ($this->required as $required) {
-            if (! array_key_exists($required, $properties)) {
-                throw new \InvalidArgumentException("Required open graph property [$required] is not present.");
-            }
-        }
-    }
+	/**
+	 * Validate to make sure the properties contain all required ones.
+	 *
+	 * @param array $properties
+	 */
+	protected function validateProperties($properties)
+	{
+		foreach ($this->required as $required) {
+			if (!array_key_exists($required, $properties)) {
+				throw new \InvalidArgumentException("Required open graph property [$required] is not present.");
+			}
+		}
+	}
 }
