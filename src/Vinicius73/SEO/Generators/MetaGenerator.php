e9355eef7d7e6fb41d9946daf2933fe2f4057f8d
Vinicius <luiz.vinicius73@gmail.com>
2013-12-30 12:56:58 -0200

    reident

--- src/Vinicius73/SEO/Generators/MetaGenerator.php
+++ src/Vinicius73/SEO/Generators/MetaGenerator.php
@@ -4,188 +4,188 @@ use Vinicius73\SEO\Contracts\MetaAware;
 
 class MetaGenerator
 {
-    /**
-     * The meta title.
-     *
-     * @var string
-     */
-    protected $title;
+	/**
+	 * The meta title.
+	 *
+	 * @var string
+	 */
+	protected $title;
 
-    /**
-     * The meta description.
-     *
-     * @var string
-     */
-    protected $description;
+	/**
+	 * The meta description.
+	 *
+	 * @var string
+	 */
+	protected $description;
 
-    /**
-     * The meta keywords.
-     *
-     * @var string
-     */
-    protected $keywords;
+	/**
+	 * The meta keywords.
+	 *
+	 * @var string
+	 */
+	protected $keywords;
 
-    /**
-     * The default configurations.
-     *
-     * @var array
-     */
-    protected $defaults = array(
-        'title' => false,
-        'description' => false,
-        'separator' => ' | ',
-        'keywords' => false
-    );
+	/**
+	 * The default configurations.
+	 *
+	 * @var array
+	 */
+	protected $defaults = array(
+		'title'       => false,
+		'description' => false,
+		'separator'   => ' | ',
+		'keywords'    => false
+	);
 
-    /**
-     * Create a new MetaGenerator instance.
-     *
-     * @param array $defaults
-     */
-    public function __construct(array $defaults = array())
-    {
-        foreach ($defaults as $key => $value) {
-            $this->defaults[$key] = $value;
-        }
-    }
+	/**
+	 * Create a new MetaGenerator instance.
+	 *
+	 * @param array $defaults
+	 */
+	public function __construct(array $defaults = array())
+	{
+		foreach ($defaults as $key => $value) {
+			$this->defaults[$key] = $value;
+		}
+	}
 
-    /**
-     * Render the meta tags.
-     *
-     * @return string
-     */
-    public function generate()
-    {
-        $title = $this->getTitle();
-        $description = $this->getDescription();
-        $keywords = $this->getKeywords();
+	/**
+	 * Render the meta tags.
+	 *
+	 * @return string
+	 */
+	public function generate()
+	{
+		$title       = $this->getTitle();
+		$description = $this->getDescription();
+		$keywords    = $this->getKeywords();
 
-        $html[] = "<title>$title</title>";
-        $html[] = "<meta name='description' itemprop='description' content='$description' />";
+		$html[] = "<title>$title</title>";
+		$html[] = "<meta name='description' itemprop='description' content='$description' />";
 
-        if (! empty($keywords)) {
-            $html[] = "<meta name='keywords' content='{$keywords}' />";
-        }
+		if (!empty($keywords)) {
+			$html[] = "<meta name='keywords' content='{$keywords}' />";
+		}
 
-        return implode(PHP_EOL, $html);
-    }
+		return implode(PHP_EOL, $html);
+	}
 
-    /**
-     * Use the meta data of a MetaAware object.
-     *
-     * @param MetaAware $object
-     */
-    public function fromObject(MetaAware $object)
-    {
-        $data = $object->getMetaData();
+	/**
+	 * Use the meta data of a MetaAware object.
+	 *
+	 * @param MetaAware $object
+	 */
+	public function fromObject(MetaAware $object)
+	{
+		$data = $object->getMetaData();
 
-        if (array_key_exists('title', $data)) {
-            $this->setTitle($data['title']);
-        }
+		if (array_key_exists('title', $data)) {
+			$this->setTitle($data['title']);
+		}
 
-        if (array_key_exists('description', $data)) {
-            $this->setDescription($data['description']);
-        }
+		if (array_key_exists('description', $data)) {
+			$this->setDescription($data['description']);
+		}
 
-        if (array_key_exists('keywords', $data)) {
-            $this->setKeywords($data['keywords']);
-        }
-    }
+		if (array_key_exists('keywords', $data)) {
+			$this->setKeywords($data['keywords']);
+		}
+	}
 
-    /**
-     * Set the Meta title.
-     *
-     * @param string $title
-     */
-    public function setTitle($title)
-    {
-        $title = strip_tags($title);
+	/**
+	 * Set the Meta title.
+	 *
+	 * @param string $title
+	 */
+	public function setTitle($title)
+	{
+		$title = strip_tags($title);
 
-        $this->title = $title . $this->getDefault('separator') . $this->getDefault('title');
-    }
+		$this->title = $title . $this->getDefault('separator') . $this->getDefault('title');
+	}
 
-    /**
-     * Set the Meta description.
-     *
-     * @param string $description
-     */
-    public function setDescription($description)
-    {
-        $description = strip_tags($description);
+	/**
+	 * Set the Meta description.
+	 *
+	 * @param string $description
+	 */
+	public function setDescription($description)
+	{
+		$description = strip_tags($description);
 
-        if (strlen($description) > 160) {
-            $description = substr($description, 0, 160);
-        }
+		if (strlen($description) > 160) {
+			$description = substr($description, 0, 160);
+		}
 
-        $this->description = $description;
-    }
+		$this->description = $description;
+	}
 
-    /**
-     * Set the Meta keywords.
-     *
-     * @param string $keywords
-     */
-    public function setKeywords($keywords)
-    {
-        $this->keywords = $keywords;
-    }
+	/**
+	 * Set the Meta keywords.
+	 *
+	 * @param string $keywords
+	 */
+	public function setKeywords($keywords)
+	{
+		$this->keywords = $keywords;
+	}
 
-    /**
-     * Get the Meta title.
-     *
-     * @return string
-     */
-    public function getTitle()
-    {
-        return $this->title ?: $this->getDefault('title');
-    }
+	/**
+	 * Get the Meta title.
+	 *
+	 * @return string
+	 */
+	public function getTitle()
+	{
+		return $this->title ? : $this->getDefault('title');
+	}
 
-    /**
-     * Get the Meta keywords.
-     *
-     * @return string
-     */
-    public function getKeywords()
-    {
-        return $this->keywords ?: $this->getDefault('keywords');
-    }
+	/**
+	 * Get the Meta keywords.
+	 *
+	 * @return string
+	 */
+	public function getKeywords()
+	{
+		return $this->keywords ? : $this->getDefault('keywords');
+	}
 
-    /**
-     * Get the Meta description.
-     *
-     * @return string
-     */
-    public function getDescription()
-    {
-        return $this->description ?: $this->getDefault('description');
-    }
+	/**
+	 * Get the Meta description.
+	 *
+	 * @return string
+	 */
+	public function getDescription()
+	{
+		return $this->description ? : $this->getDefault('description');
+	}
 
-    /**
-     * Reset the title and description fields.
-     *
-     * @return void
-     */
-    public function reset()
-    {
-        $this->title = null;
-        $this->description = null;
-        $this->keywords = null;
-    }
+	/**
+	 * Reset the title and description fields.
+	 *
+	 * @return void
+	 */
+	public function reset()
+	{
+		$this->title       = null;
+		$this->description = null;
+		$this->keywords    = null;
+	}
 
-    /**
-     * Get a default configuration.
-     *
-     * @param string $default
-     *
-     * @return mixed
-     */
-    public function getDefault($default)
-    {
-        if (array_key_exists($default, $this->defaults)) {
-            return $this->defaults[$default];
-        }
+	/**
+	 * Get a default configuration.
+	 *
+	 * @param string $default
+	 *
+	 * @return mixed
+	 */
+	public function getDefault($default)
+	{
+		if (array_key_exists($default, $this->defaults)) {
+			return $this->defaults[$default];
+		}
 
-        $class = get_class($this);
-        throw new \InvalidArgumentException("{$class}: default configuration $default does not exist.");
-    }
+		$class = get_class($this);
+		throw new \InvalidArgumentException("{$class}: default configuration $default does not exist.");
+	}
 }
